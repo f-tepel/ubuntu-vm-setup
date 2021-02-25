ce01af2a170b8a0d4874f568d12e98006c00e8f0
Felix <felix.tepel@gmail.com>
2021-02-25 16:04:49 +0100

    Change docker install

--- setup_commands.sh
+++ setup_commands.sh
@@ -17,19 +17,16 @@ ufw enable
 ufw status
 
 # Install docker https://docs.docker.com/engine/install/ubuntu/
-sudo apt install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg-agent \
-  software-properties-common
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker admin
+sudo usermod -aG docker git
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+# Docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+# Git
+sudo apt install git
